e94e245ca09d5aa7f87d3ec22a2c2b4d5ab3d13a
Samiksha <shende.samiksha@rediffmail.com>
2021-03-30 16:06:07 +0530

    [Samiksha].Add code to create binary search tree and pass the size test

--- src/main/java/com/bridgelabz/binarytree/MyBinaryNode.java
+++ src/main/java/com/bridgelabz/binarytree/MyBinaryNode.java
@@ -1,2 +1,13 @@
-package com.bridgelabz.binarytree;public class MyBinaryNode {
+package com.bridgelabz.binarytree;
+
+public class MyBinaryNode<K extends Comparable<K>> {
+    K key;
+    MyBinaryNode<K> left;
+    MyBinaryNode<K> right;
+
+    public MyBinaryNode(K key) {
+        this.key = key;
+        this.left = null;
+        this.right = null;
+    }
 }
